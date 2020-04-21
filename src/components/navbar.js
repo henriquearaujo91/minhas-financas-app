bd17a7460c570645e1c2c467787462c9a7d9dc87
henriquearaujo91 <henrique_araujo91@outlook.com>
2020-04-21 10:46:40 -0300

    Adicionando as rotas no NavBar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import NavbarItem from "./navberItem";
 
 function Navbar() {
   return (
@@ -20,26 +21,10 @@ function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="home.html">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="usuarios.html">
-                Usuários
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="lancamentos.html">
-                Lançamentos
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="login.html">
-                Login
-              </a>
-            </li>
+            <NavbarItem href="#/" label="Home" />
+            <NavbarItem href="#/cadastro-usuarios" label="Usuários" />
+            <NavbarItem href="#/" label="Lançamentos" />
+            <NavbarItem href="#/login" label="Login" />
           </ul>
         </div>
       </div>
