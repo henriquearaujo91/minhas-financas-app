8a85361023dd05097a37164be5185316e6b9d50e
henriquearaujo91 <henrique_araujo91@outlook.com>
2020-04-22 17:05:40 -0300

    Implementando o Datatable de lancamentos

--- src/views/lancamentos/consultaLancamentos.js
+++ src/views/lancamentos/consultaLancamentos.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import Card from "../components/card";
-import FormGroup from "../components/form-group";
-import SelectMenu from "../components/selectMenu";
+import Card from "../../components/card";
+import FormGroup from "../../components/form-group";
+import SelectMenu from "../../components/selectMenu";
+import LancamentosTable from "./lancamentosTable";
 
 class ConsultaLancamentos extends React.Component {
   render() {
@@ -28,6 +29,17 @@ class ConsultaLancamentos extends React.Component {
       { label: "Receita", value: "RECEITA" },
     ];
 
+    const lancamentos = [
+      {
+        id: 1,
+        descricao: "salario",
+        valor: 5000,
+        tipo: "Receita",
+        mes: 1,
+        status: "Efetivado",
+      },
+    ];
+
     return (
       <Card title="Consulta Lancamentos">
         <div className="row">
@@ -48,7 +60,19 @@ class ConsultaLancamentos extends React.Component {
               <FormGroup label="Tipo de Lançamento: " htmlFor="inputTipo">
                 <SelectMenu className="form-control" lista={tipos} />
               </FormGroup>
+              <button type="button" className="btn btn-success">
+                Buscar
+              </button>
+              <button type="button" className="btn btn-danger">
+                Cadastrar
+              </button>
             </div>
+          </div>
+        </div>
+        <br />
+        <div className="rol">
+          <div className="bs-component">
+            <LancamentosTable lancamentos={lancamentos} />
           </div>
         </div>
       </Card>
