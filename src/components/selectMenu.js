2520596955c3d8da8b1261e1961ed1c0284465bc
henriquearaujo91 <henrique_araujo91@outlook.com>
2020-04-22 16:33:45 -0300

    Criando o componente Select Menu (combo box)

---
+++ src/components/selectMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default (props) => {
+  const options = props.lista.map((option) => {
+    return <option value={option.value}>{option.label}</option>;
+  });
+
+  return (
+    <select {...props}>
+      {options}
+    </select>
+  );
+};
