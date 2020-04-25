4c48bf37a4a5bf6cee8e385c5cb32307af9efa48
henriquearaujo91 <henrique_araujo91@outlook.com>
2020-04-25 09:46:02 -0300

    Criando o formulário de cadastro e atualização de Lançamentos

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -4,6 +4,7 @@ import Login from "../views/login";
 import CadastroUsuario from "../views/cadastroUsuario";
 import Home from "../views/home";
 import ConsultaLancamentos from "../views/lancamentos/consultaLancamentos";
+import CadastroLancamentos from "../views/lancamentos/cadastroLancamentos";
 
 function Rotas() {
   return (
@@ -13,6 +14,7 @@ function Rotas() {
         <Route path="/home" component={Home} />
         <Route path="/cadastro-usuarios" component={CadastroUsuario} />
         <Route path="/consulta-lancamentos" component={ConsultaLancamentos} />
+        <Route path="/cadastro-lancamentos" component={CadastroLancamentos} />
       </Switch>
     </HashRouter>
   );
