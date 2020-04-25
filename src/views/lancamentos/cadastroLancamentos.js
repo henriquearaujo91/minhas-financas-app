330b9377a06cd58bb416069bb094a34a2910633b
henriquearaujo91 <henrique_araujo91@outlook.com>
2020-04-25 09:57:01 -0300

    Adicionando o estado no cadastro de lançamentos e o método handleChange

--- src/views/lancamentos/cadastroLancamentos.js
+++ src/views/lancamentos/cadastroLancamentos.js
@@ -1,17 +1,36 @@
 import React from "react";
-
-import Card from "../../components/card";
-
 import { withRouter } from "react-router-dom";
+import LancamentoService from "../../app/service/lancamentoService";
+import Card from "../../components/card";
 import FormGroup from "../../components/form-group";
 import SelectMenu from "../../components/selectMenu";
-import LancamentoService from "../../app/service/lancamentoService";
 
 class CadastroLancamentos extends React.Component {
+  state = {
+    id: null,
+    descricao: "",
+    mes: "",
+    ano: "",
+    valor: "",
+    tipo: "",
+    status: "",
+  };
   constructor() {
     super();
     this.service = new LancamentoService();
   }
+
+  submit = () => {
+    console.log(this.state);
+  };
+
+  handleChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+
+    this.setState({ [name]: value });
+  };
+
   render() {
     const tipos = this.service.obterListaTipos();
     const meses = this.service.obterListaMeses();
@@ -21,14 +40,28 @@ class CadastroLancamentos extends React.Component {
         <div className="row">
           <div className="col-md-12">
             <FormGroup id="inputDescricao" label="Descrição: *">
-              <input id="inputDescricao" type="text" className="form-control" />
+              <input
+                id="inputDescricao"
+                type="text"
+                className="form-control"
+                name="descricao"
+                value={this.state.descricao}
+                onChange={this.handleChange}
+              />
             </FormGroup>
           </div>
         </div>
         <div className="row">
           <div className="col-md-6">
             <FormGroup id="inputAno" label="Ano: *">
-              <input id="inputAno" type="text" className="form-control" />
+              <input
+                id="inputAno"
+                type="text"
+                className="form-control"
+                name="ano"
+                value={this.state.ano}
+                onChange={this.handleChange}
+              />
             </FormGroup>
           </div>
           <div className="col-md-6">
@@ -37,6 +70,9 @@ class CadastroLancamentos extends React.Component {
                 id="inputMes"
                 lista={meses}
                 className="form-control"
+                name="mes"
+                value={this.state.mes}
+                onChange={this.handleChange}
               />
             </FormGroup>
           </div>
@@ -44,7 +80,14 @@ class CadastroLancamentos extends React.Component {
         <div className="row">
           <div className="col-md-4">
             <FormGroup id="inputValor" label="Valor: *">
-              <input id="inputValor" type="text" className="form-control" />
+              <input
+                id="inputValor"
+                type="text"
+                className="form-control"
+                name="valor"
+                value={this.state.valor}
+                onChange={this.handleChange}
+              />
             </FormGroup>
           </div>
           <div className="col-md-4">
@@ -53,19 +96,30 @@ class CadastroLancamentos extends React.Component {
                 id="inputTipo"
                 lista={tipos}
                 className="form-control"
+                name="tipo"
+                value={this.state.tipo}
+                onChange={this.handleChange}
               />
             </FormGroup>
           </div>
           <div className="col-md-4">
             <FormGroup id="inputStatus" label="Status: ">
-              <input type="text" className="form-control" disabled />
+              <input
+                type="text"
+                className="form-control"
+                disabled
+                name="status"
+                value={this.state.status}
+              />
             </FormGroup>
           </div>
         </div>
         <div className="row">
           <div className="col-md-6">
             <div>
-              <button className="btn btn-success">Salvar</button>
+              <button className="btn btn-success" onClick={this.submit}>
+                Salvar
+              </button>
               <button className="btn btn-danger">Cancelar</button>
             </div>
           </div>
