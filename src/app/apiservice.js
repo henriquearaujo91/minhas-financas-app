d6a1fbe74b5d83e1817961b2423ec7c26f5e5b69
henriquearaujo91 <henrique_araujo91@outlook.com>
2020-04-26 13:08:42 -0300

    Fazendo o deploy da aplicação React no Heroku

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://minhasfinancas-app-curso.herokuapp.com",
 });
 
 class ApiService {
